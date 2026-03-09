6e0b067da00c9322ca567d7e6f08ad8ac82a5c89
alexrivas05 <alex12rivas34@gmail.com>
2026-03-08 23:16:39 -0400

    added allwords and finished wotd

--- routes/words.js
+++ routes/words.js
@@ -13,6 +13,56 @@ router.get('/wotd', async (req, res)=>{
     res.render('wotd',{word:word, part:part, definition:definition});
 });
 
+router.get('/allwords', async (req, res) => {
+    try {
+        const data = await readFile('resources/allwords.txt', 'utf8');
+
+        let lines = data.split('\n');
+
+        let words = [];
+        let parts = [];
+        let definitions = [];
+
+        for(let i = 0; i < lines.length; i++){
+            let pieces = lines[i].split('\t');
+
+            words.push(pieces[0]);
+            parts.push(pieces[1]);
+            definitions.push(pieces[2]);
+        }
+
+        for(let i = 0; i < words.length; i++){
+            for(let j = i + 1; j < words.length; j++){
+
+                if(words[i].localeCompare(words[j]) > 0){
+
+                    let tempWord = words[i];
+                    words[i] = words[j];
+                    words[j] = tempWord;
+
+                    let tempPart = parts[i];
+                    parts[i] = parts[j];
+                    parts[j] = tempPart;
+
+                    let tempDef = definitions[i];
+                    definitions[i] = definitions[j];
+                    definitions[j] = tempDef;
+                }
+            }
+        }
+
+        res.render('allwords',{
+            words: words,
+            parts: parts,
+            definitions: definitions
+        });
+
+    } catch(err){
+        console.log("Error reading words:", err);
+        res.send("Error loading words");
+    }
+});
+
 let getWordFromDictionary = async ()=>{
     try{
         const data = await readFile('resources/allwords.txt', 'utf8');
